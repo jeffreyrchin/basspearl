67a977d7c32aa3d22a1ba15caa6da34f69da1870
jeffreyrchin <jeffreyrchin.20@gmail.com>
2026-03-08 13:22:15 -0700

    Make the 3D world transparent (play nice with melded layers)

--- services/ThreeJSRenderer.ts
+++ services/ThreeJSRenderer.ts
@@ -152,7 +152,7 @@ export class ThreeJSRenderer {
 
         gl.colorMask(true, true, true, true);
         gl.depthMask(true);
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clearColor(0.0, 0.0, 0.0, 0.0);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
 
         try {
