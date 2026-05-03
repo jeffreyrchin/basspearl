1276acbf9ff26b0b4309eb24bd51ce91466e33f0
jeffreyrchin <jeffreyrchin.20@gmail.com>
2026-05-02 19:48:45 -0700

    Fix z-index layering for auth and pro modals

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -104,7 +104,7 @@ const AuthModal = () => {
     };
 
     return (
-        <div className="fixed inset-0 z-modal flex items-center justify-center p-4">
+        <div className="fixed inset-0 z-auth-modal flex items-center justify-center p-4">
             {/* Backdrop */}
             <motion.div
                 initial={{ opacity: 0 }}
