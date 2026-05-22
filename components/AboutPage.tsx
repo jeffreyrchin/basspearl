5670886f26f3b5fc664139c72d6efdf0f01fb9eb
jeffreyrchin <jeffreyrchin.20@gmail.com>
2026-05-22 13:44:39 -0700

    Fix footer not being at the bottom of about page for larger screens

--- components/AboutPage.tsx
+++ components/AboutPage.tsx
@@ -12,7 +12,7 @@ const AboutPage = () => {
             >
                 <Navbar />
 
-                <main className="flex-none relative grid-bg">
+                <main className="grow relative grid-bg">
                     <div className="absolute top-1/4 left-1/2 -translate-x-1/2 w-[600px] h-[600px] bg-primary/5 rounded-full blur-[120px] pointer-events-none" />
 
                     <div className="relative z-10 max-w-7xl mx-auto px-4 md:px-8 py-12 md:py-20">
